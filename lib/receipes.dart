123e066f7f7d1348c65a73e777b0e25ad91a998a
Patel <vp185104@ncr.com>
2019-04-04 14:32:51 +0530

    Receipe UI

--- lib/receipes.dart
+++ lib/receipes.dart
@@ -1,8 +1,28 @@
 import 'package:circular_check_box/circular_check_box.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+
+List<Image> _ingriImages = [
+  Image.asset(
+    'assets/tomatoes.jpg',
+    fit: BoxFit.cover,
+  ),
+  Image.asset(
+    'assets/potatoes.jpg',
+    fit: BoxFit.cover,
+  ),
+  Image.asset(
+    'assets/pizza_base.jpg',
+    fit: BoxFit.cover,
+  ),
+  Image.asset(
+    'assets/onions.jpg',
+    fit: BoxFit.cover,
+  ),
+];
 
 class Receipes extends StatefulWidget {
   @override
@@ -24,44 +44,6 @@ class _ReceipesState extends State<Receipes> {
       fit: BoxFit.cover,
     ),
   ];
-
-  List<Image> _ingriImages = [
-    Image.asset(
-      'assets/tomatoes.jpg',
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      'assets/potatoes.jpg',
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      'assets/pizza_base.jpg',
-      fit: BoxFit.cover,
-    ),
-    Image.asset(
-      'assets/onions.jpg',
-      fit: BoxFit.cover,
-    ),
-  ];
-
-//  List<Text> _ingriNames = [
-//    Text(
-//      'Onions',
-//      style: TextStyle(color: Colors.black),
-//    ),
-//    Text(
-//      'Pizza base',
-//      style: TextStyle(color: Colors.black),
-//    ),
-//    Text(
-//      'Potatoes',
-//      style: TextStyle(color: Colors.black),
-//    ),
-//    Text(
-//      'Tomatoes',
-//      style: TextStyle(color: Colors.black),
-//    ),
-//  ];
 
   List<Text> _foodNames = [
     Text(
@@ -154,11 +136,19 @@ class _ReceipesState extends State<Receipes> {
                 builder: (BuildContext context) {
                   return Stack(
                     children: <Widget>[
-                      Container(
-                        width: MediaQuery.of(context).size.width,
-                        margin: EdgeInsets.symmetric(horizontal: 5.0),
-                        decoration: BoxDecoration(color: Colors.amber),
-                        child: _images[i - 1],
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => Methods()));
+                        },
+                        child: Container(
+                          width: MediaQuery.of(context).size.width,
+                          margin: EdgeInsets.symmetric(horizontal: 5.0),
+                          decoration: BoxDecoration(color: Colors.amber),
+                          child: _images[i - 1],
+                        ),
                       ),
                       Align(
                         alignment: Alignment.bottomCenter,
@@ -226,6 +216,26 @@ class _ReceipesState extends State<Receipes> {
                                                   .updateData({
                                                 'selected': x,
                                               });
+                                              if (x) {
+                                                Firestore.instance
+                                                    .runTransaction((Transaction
+                                                        transaction) async {
+                                                  CollectionReference
+                                                      reference = Firestore
+                                                          .instance
+                                                          .collection('cart');
+                                                  await reference.add({
+                                                    "name": document['name']
+                                                  });
+                                                });
+                                                Flushbar(
+                                                  title: "Info",
+                                                  message:
+                                                      "${document['name']} added to cart",
+                                                  duration:
+                                                      Duration(seconds: 3),
+                                                )..show(context);
+                                              }
                                             });
                                           }),
                                       child: Padding(
@@ -256,5 +266,221 @@ class _ReceipesState extends State<Receipes> {
         ],
       ),
     );
+  }
+}
+
+class Methods extends StatefulWidget {
+  @override
+  _MethodsState createState() => _MethodsState();
+}
+
+class _MethodsState extends State<Methods> {
+  Text string = Text(
+    'METHOD',
+    style: TextStyle(color: Colors.black),
+  );
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: 2,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverAppBar(
+                backgroundColor: Colors.red,
+                expandedHeight: 250.0,
+                floating: false,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+                  centerTitle: true,
+                  title: Text(
+                    "Pizza",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                  background: Image.asset(
+                    "assets/food2.jpg",
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              SliverPersistentHeader(
+                delegate: _SliverAppBarDelegate(
+                  TabBar(
+                    labelColor: Colors.black87,
+                    unselectedLabelColor: Colors.grey,
+                    tabs: [
+                      Tab(icon: Icon(Icons.info), text: "METHOD"),
+                      Tab(icon: Icon(Icons.fastfood), text: "INGREDIENTS"),
+                    ],
+                  ),
+                ),
+                pinned: true,
+                floating: false,
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: <Widget>[
+              Padding(
+                padding: const EdgeInsets.all(13.0),
+                child: ListView(
+                  padding: EdgeInsets.all(8.0),
+                  children: <Widget>[
+                    Text(
+                      "1 Proof the yeast: Place the warm water in the large bowl of a heavy duty stand mixer. Sprinkle the yeast over the warm water and let it sit for 5 minutes until the yeast is dissolved. After 5 minutes stir if the yeast hasn't dissolved completely. The yeast should begin to foam or bloom, indicating that the yeast is still active and alive \n\n 2 Make and knead the pizza dough: Using the mixing paddle attachment, mix in the flour, salt, sugar, and olive oil on low speed for a minute. Then replace the mixing paddle with the dough hook attachment. Knead the pizza dough on low to medium speed using the dough hook about 7-10 minutes. The dough should be a little sticky, or tacky to the touch. If it's too wet, sprinkle in a little more flour. \n\n 3 Let the dough rise: Spread a thin layer of olive oil over the inside of a large bowl. Place the pizza dough in the bowl and turn it around so that it gets coated with the oil.At this point you can choose how long you want the dough to ferment and rise.",
+                      style: TextStyle(fontSize: 16.0),
+                    ),
+                  ],
+                ),
+              ),
+              //Ingredients page
+              StreamBuilder<QuerySnapshot>(
+                stream: Firestore.instance.collection('ingri').snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasError)
+                    return Center(
+                      child: Text('Error: ${snapshot.error}'),
+                    );
+                  if (!snapshot.hasData)
+                    return Center(
+                      child: Text('No List Found'),
+                    );
+                  switch (snapshot.connectionState) {
+                    case ConnectionState.waiting:
+                      return Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    default:
+                      return snapshot.data.documents.length > 0
+                          ? Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Column(
+                                children: <Widget>[
+                                  GridView.count(
+                                    crossAxisCount: 2,
+                                    shrinkWrap: true,
+                                    children: snapshot.data.documents
+                                        .map((DocumentSnapshot document) {
+                                      return Padding(
+                                        padding: const EdgeInsets.all(8.0),
+                                        child: GridTile(
+                                          header: Center(
+                                            child: Text(
+                                                document['name'].toString()),
+                                          ),
+                                          footer: CircularCheckBox(
+                                              activeColor: Colors.green,
+                                              value: document['selected'],
+                                              materialTapTargetSize:
+                                                  MaterialTapTargetSize.padded,
+                                              onChanged: (bool x) {
+                                                setState(() {
+                                                  Firestore.instance
+                                                      .collection('ingri')
+                                                      .document(
+                                                          document.documentID)
+                                                      .updateData({
+                                                    'selected': x,
+                                                  });
+                                                  if (x) {
+                                                    Firestore.instance
+                                                        .runTransaction((Transaction
+                                                            transaction) async {
+                                                      CollectionReference
+                                                          reference = Firestore
+                                                              .instance
+                                                              .collection(
+                                                                  'cart');
+                                                      await reference.add({
+                                                        "name": document['name']
+                                                      });
+                                                    });
+                                                    Flushbar(
+                                                      title: "Info",
+                                                      message:
+                                                          "${document['name']} added to cart",
+                                                      duration:
+                                                          Duration(seconds: 3),
+                                                    )..show(context);
+                                                  }
+                                                });
+                                              }),
+                                          child: Padding(
+                                            padding: const EdgeInsets.only(
+                                              top: 23.0,
+                                              bottom: 40.0,
+                                            ),
+                                            child: Container(
+                                              width: 50.0,
+                                              height: 20.0,
+                                              child: _ingriImages[
+                                                  document['id'] - 1],
+                                            ),
+                                          ),
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                  Expanded(
+                                    child: Align(
+                                      alignment: Alignment.bottomCenter,
+                                      child: RaisedButton(
+                                          padding: EdgeInsets.all(15.0),
+                                          shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                new BorderRadius.circular(30.0),
+                                          ),
+                                          color: Colors.blue,
+                                          child: Text(
+                                            'SELECT ALL',
+                                            style:
+                                                TextStyle(color: Colors.white),
+                                          ),
+                                          onPressed: () {}),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            )
+                          : Center(
+                              child: Text('No items in your cart'),
+                            );
+                  }
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return new Container(
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
